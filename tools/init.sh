9189476eb5dbb57bc513d49aa3f7f6b6ece9b209
JiYou <jiyou09@gmail.com>
2014-09-27 22:33:08 -0400

    "Update init-tools."
    Signed-off-by: JiYou <jiyou09@gmail.com>

--- tools/init.sh
+++ tools/init.sh
@@ -5,20 +5,19 @@ set -o xtrace
 TOP_DIR=$(cd $(dirname "$0") && pwd)
 TEMP=`mktemp`; rm -rfv $TEMP >/dev/null; mkdir -p $TEMP;
 DEST=/opt/stack
-source $TOP_DIR/tools/function
-cp -rf $TOP_DIR/`ls -l $TOP_DIR/localrc | awk '{print $11}'` /root/
+source ./function
 #---------------------------------------------
 # Check for apt.
 #---------------------------------------------
 DEBIAN_FRONTEND=noninteractive \
 apt-get --option \
 "Dpkg::Options::=--force-confold" --assume-yes \
-install -y --force-yes openssh-server 
+install -y --force-yes openssh-server
 #---------------------------------------------
 # Kill process by Name
 #---------------------------------------------
 
-cp -rf $TOP_DIR/tools/nkill /usr/bin/
+cp -rf ./nkill /usr/bin/
 chmod +x /usr/bin/nkill
 
 #---------------------------------------------

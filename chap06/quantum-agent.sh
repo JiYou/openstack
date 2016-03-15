3addc91540a64de9fdf219baf05c28a1b2ab871d
youji <youji@ebay.com>
2016-03-15 16:53:15 -0700

    update quantum agent start script.
    Signed-off-by: youji <youji@ebay.com>

--- chap06/quantum-agent.sh
+++ chap06/quantum-agent.sh
@@ -122,7 +122,7 @@ nkill quantum-openvswitch-agent
 mkdir -p /var/log/quantum/
 rm -rf /var/log/quantum/quantum*
 
-nohup python /opt/stack/quantum/bin/quantum-openvswitch-agent --config-file /etc/quantum/quantum.conf --config-file=/etc/quantum/l3_agent.ini > /var/log/quantum/quantum-openvswitch-agent.log 2>&1 &
+nohup python /opt/stack/quantum/bin/quantum-openvswitch-agent --config-file /etc/quantum/quantum.conf --config-file=/etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini > /var/log/quantum/quantum-openvswitch-agent.log 2>&1 &
 
 EOF
 
